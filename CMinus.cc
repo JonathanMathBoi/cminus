809026463f8db4f3c8251b6e9590b036244de5f0
Jonathan Hart <jonathan.e.hart@outlook.com>
2024-03-04 16:10:32 -0500

    Removed unnecessary functions from CMinus

--- CMinus.cc
+++ CMinus.cc
@@ -2,58 +2,8 @@
 #include "Lexer.hpp"
 #include "Parser.hpp"
 
-#include <format>
 #include <fstream>
-#include <iomanip>
 #include <iostream>
-#include <sstream>
-#include <string>
-
-/***********************************************************************/
-
-std::string
-token_value (Token tok)
-{
-    switch (tok.type) {
-    case ID:
-        return tok.lexeme;
-    case NUM:
-        return std::to_string (tok.number);
-    default:
-        return "";
-    }
-}
-
-std::string
-quote (std::string str)
-{
-    std::stringstream ss;
-    ss << std::quoted (str);
-    std::string quoted;
-    ss >> quoted;
-    return quoted;
-}
-
-void
-print_token (Token tok, int line_num, int col_num)
-{
-    if (tok.type == ERROR) {
-        std::cout << std::format (
-                "{:20s}{:20s}Line: {:d}; Column: {:d}",
-                token_types.at(tok.type),
-                quote (tok.lexeme),
-                line_num,
-                col_num
-            ) << std::endl;
-    } else {
-        std::cout << std::format(
-                "{:20s}{:20s}{:s}", 
-                token_types.at (tok.type),
-                quote (tok.lexeme),
-                token_value (tok)
-            ) << std::endl;
-    }
-}
 
 /***********************************************************************/
 
@@ -76,6 +26,4 @@ int main (int argc, char* argv[]) {
         std::cout << exception.what() << std::endl;
     }
 }
-
-
 
