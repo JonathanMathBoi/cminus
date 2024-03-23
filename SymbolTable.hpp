2f0606868ea1b598aec7506d73c9d0de972d9dd8
Jonathan Hart <jonathan.e.hart@outlook.com>
2024-03-23 17:29:48 -0400

    Added SymbolTable header

--- SymbolTable.hpp
+++ SymbolTable.hpp
@@ -6,9 +6,33 @@
 #include "AST.hpp"
 
 #include <memory>
+#include <optional>
 #include <string>
+#include <string_view>
 #include <unordered_map>
 #include <vector>
+
+/***********************************************************************/
+
+using ScopeTable =
+    std::unordered_map<std::string, std::shared_ptr<DeclarationNode>>;
+
+/***********************************************************************/
+
+class SymbolTable {
+public:
+    SymbolTable();
+
+    void enterScope();
+    bool insert(std::shared_ptr<DeclarationNode> node);
+    std::optional<std::shared_ptr<DeclarationNode>> lookup(
+        const std::string_view name) const;
+    unsigned getNestLevel() const;
+
+private:
+    unsigned m_nestLevel;
+    std::vector<ScopeTable> m_table;
+};
 
 /***********************************************************************/
 
