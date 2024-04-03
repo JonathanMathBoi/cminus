6f14c18dd815ecc540f75c019b40c607936975c7
Jonathan Hart <jonathan.e.hart@outlook.com>
2024-04-03 15:13:05 -0400

    Added CodegenVisitor Header

---
+++ src/codegen/CodegenVisitor.hpp
@@ -0,0 +1,64 @@
+#ifndef CODEGENVISITOR_HPP
+#define CODEGENVISITOR_HPP
+
+/***********************************************************************/
+
+#include "../ast/AST.hpp"
+
+#include <memory>
+
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+
+/***********************************************************************/
+
+class CodegenVisitor : public Visitor {
+public:
+    /// \brief Constructs a CodegenVisitor with the given LLVM context and
+    /// module
+    ///
+    /// \param context the LLVM context
+    /// \param module the module to generate code in
+    CodegenVisitor(
+        std::shared_ptr<llvm::LLVMContext> context,
+        std::shared_ptr<llvm::Module> module);
+
+public:
+    virtual void visit(ProgramNode& node) override;
+
+    virtual void visit(FunctionDeclarationNode& node) override;
+    virtual void visit(VariableDeclarationNode& node) override;
+    virtual void visit(ArrayDeclarationNode& node) override;
+    virtual void visit(ParameterNode& node) override;
+
+    virtual void visit(CompoundStatementNode& node) override;
+    virtual void visit(IfStatementNode& node) override;
+    virtual void visit(WhileStatementNode& node) override;
+    virtual void visit(ReturnStatementNode& node) override;
+    virtual void visit(ExpressionStatementNode& node) override;
+
+    virtual void visit(AssignmentExpressionNode& node) override;
+    virtual void visit(VariableExpressionNode& node) override;
+    virtual void visit(SubscriptExpressionNode& node) override;
+    virtual void visit(CallExpressionNode& node) override;
+    virtual void visit(AdditiveExpressionNode& node) override;
+    virtual void visit(MultiplicativeExpressionNode& node) override;
+    virtual void visit(RelationalExpressionNode& node) override;
+    virtual void visit(IntegerLiteralExpressionNode& node) override;
+    virtual void visit(FloatLiteralExpressionNode& node) override;
+    virtual void visit(BoolLiteralExpressionNode& node) override;
+
+private:
+    /// Generates the code for the compiler builtin functions
+    void codegenBuiltins();
+
+private:
+    std::shared_ptr<llvm::LLVMContext> m_context;
+    std::unique_ptr<llvm::IRBuilder<>> m_irBuilder;
+    std::shared_ptr<llvm::Module> m_module;
+};
+
+/***********************************************************************/
+
+#endif
