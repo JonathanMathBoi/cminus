1c2c640d4f2f3566764ff9d04ab672b04e3a0f34
Jonathan Hart <jonathan.e.hart@outlook.com>
2024-04-10 15:11:06 -0400

    Moved cmd args to their own function

--- src/CMinus.cpp
+++ src/CMinus.cpp
@@ -16,6 +16,7 @@
 #include <boost/program_options/parsers.hpp>
 #include <boost/program_options/positional_options.hpp>
 #include <boost/program_options/value_semantic.hpp>
+#include <boost/program_options/variables_map.hpp>
 #include <filesystem>
 #include <fstream>
 #include <ios>
@@ -29,35 +30,12 @@ namespace po = boost::program_options;
 
 /***********************************************************************/
 
+po::variables_map getCmdArgs(int argc, char* argv[]);
+
+/***********************************************************************/
+
 int main(int argc, char* argv[]) {
-    // Define command line options
-    po::options_description desc("Options");
-    desc.add_options()("help,h", "Produce help message")(
-        "optimize,O", po::value<int>()->default_value(0),
-        "Optimization level (0, 1, 2, or 3)")(
-        "input-file", po::value<std::string>(), "Input file");
-
-    po::positional_options_description p;
-    p.add("input-file", -1);
-
-    // Parse the command line
-    po::variables_map vm;
-    po::store(
-        po::command_line_parser(argc, argv).options(desc).positional(p).run(),
-        vm);
-    po::notify(vm);
-
-    // Handle help option
-    if (vm.count("help")) {
-        std::cout << desc << std::endl;
-        return 1;
-    }
-
-    // Check if input file is provided
-    if (!vm.count("input-file")) {
-        std::cerr << "Error: Input file not specified!" << std::endl;
-        return 1;
-    }
+    po::variables_map vm {getCmdArgs(argc, argv)};
 
     std::string input_file {vm["input-file"].as<std::string>()};
 
@@ -108,3 +86,40 @@ int main(int argc, char* argv[]) {
     ast->accept(codegen);
     module->print(llvm::errs(), /*AAW=*/nullptr);
 }
+
+/***********************************************************************/
+
+po::variables_map getCmdArgs(int argc, char* argv[]) {
+    // Define command line options
+    po::options_description desc("Options");
+    desc.add_options()("help,h", "Produce help message")(
+        "optimize,O", po::value<int>()->default_value(0),
+        "Optimization level (0, 1, 2, or 3)")(
+        "input-file", po::value<std::string>(), "Input file");
+
+    po::positional_options_description p;
+    p.add("input-file", -1);
+
+    // Parse the command line
+    po::variables_map vm;
+    po::store(
+        po::command_line_parser(argc, argv).options(desc).positional(p).run(),
+        vm);
+    po::notify(vm);
+
+    // Handle help option
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        std::exit(1);
+    }
+
+    // Check if input file is provided
+    if (!vm.count("input-file")) {
+        std::cerr << "Error: Input file not specified!" << std::endl;
+        std::exit(1);
+    }
+
+    return vm;
+}
+
+/***********************************************************************/
